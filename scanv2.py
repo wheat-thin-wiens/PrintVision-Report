6c8c6f7e684df1bfbbea588ed0d19d2b0b60be67
ethan wiens <ethanjwiens@gmail.com>
2024-07-10 16:28:53 -0500

    finish v.0.2.0

--- scanv2.py
+++ scanv2.py
@@ -1,26 +1,34 @@
 # PrintVision Report by Ethan Wiens
 import csv
+import os
 import os.path
+import time
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import filedialog as fd
 from tkinter import ttk
 
 root = tk.Tk()
-root.geometry("400x210")
+root.geometry("400x280")
 root.title("PrintVision Report")
+root.iconbitmap('printing.ico')
 
 def open_file():
+    global filename
     filename = fd.askopenfilename(
-        title = "Open a file",
-        initialdir = '/Downloads',
+        title = "Open file",
+        initialdir = '/',
         filetypes = (
             ('CSV Files', '*.csv'),
             #('All Files', '*.*'),
         )
     )
 
-    fileVar.set(f"{os.path.basename(filename)}")
-    runBtn.config(state = 'normal' if fileVar != "No file selected" else 'disabled')
+    if len(filename) > 0:
+        fileVar.set(f"{os.path.basename(filename)}")
+        runBtn.config(state = 'normal' if fileVar != "No file selected" else 'disabled')
+    else:
+        return
 
 def hospital_report():
     value = int(hsptlValue.get())
@@ -176,8 +184,8 @@ def clinic_report():
                     continue
 
 def run_report(location):
-    os.chdir('/Users/ethanwiens/Downloads')
-    #os.chdir('/Downloads')
+    #os.chdir('/Users/ethanwiens/Downloads')
+    os.chdir('C:/Users/Public')
 
     if os.path.isfile('report.csv'):
         os.remove('report.csv')
@@ -191,30 +199,48 @@ def run_report(location):
         hospital_report()
         clinic_report()
 
+    time.sleep(1)
     statusVar.set("Report complete.")
 
+    outLocation = fd.askdirectory(
+        title = "Save new report",
+        initialdir = "/"
+    )
+
+    if len(outLocation) > 0:
+        outVar.set(f"{outLocation}")
+        os.replace('C:/Users/Public/report.csv', f"{outLocation}/report.csv")
+    else:
+        os.remove("C:/Users/Public/report.csv")
+
+    #messagebox.askyesno("Report Complete", "Would you like to view the report?")
+
 def create_gui():
-    global filename, fileVar, runBtn, statusVar, hsptlValue, clncValue
+    global fileVar, runBtn, statusVar, hsptlValue, clncValue, outVar
 
     fileVar = tk.StringVar(value = "No file selected")
     statusVar = tk.StringVar(value = "Ready")
-    hsptlValue = tk.StringVar(value = '0')
-    clncValue = tk.StringVar(value = '0')
+    hsptlValue = tk.StringVar(value = '5')
+    clncValue = tk.StringVar(value = '10')
+    outVar = tk.StringVar(value = 'No destination selected')
 
     # Open Button
     openBtn = tk.Button(root, text = "Open File", command = open_file)
-    openBtn.grid(row = 4, column = 2, padx = 10, pady = 10, sticky = 'se')
+    openBtn.grid(row = 5, column = 1, padx = 10, pady = 10, sticky = 'se')
 
     # Run Button
     runBtn = tk.Button(root, text = "Run", state = 'disabled', command = lambda:run_report(location_dropdown.get()))
-    runBtn.grid(row = 4, column = 3, padx = 10, pady = 10, sticky = 'se')
+    runBtn.grid(row = 5, column = 2, padx = 10, pady = 10, sticky = 'se')
 
     # Input Frame
     inputFrame = tk.Frame(root)
-    inputFrame.grid(row = 0, column = 0, padx = 10, pady = 10, sticky = 'nwe')
+    inputFrame['borderwidth'] = 5
+    inputFrame['relief'] = 'groove'
+    inputFrame.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10, sticky = 'nwe')
 
     # Location Select
     location_dropdown = ttk.Combobox(
+        inputFrame,
         state = "readonly",
         values = ['', 'Hospital', 'Clinics', 'All'],
         width = 20,
@@ -222,38 +248,42 @@ def create_gui():
     location_dropdown.current(0)
     location_dropdown.grid(row = 0, column = 1, padx = 10, pady = 10, sticky = 'nw')
 
-    location_label = tk.Label(root, text = "Location")
+    location_label = tk.Label(inputFrame, text = "Location:")
     location_label.grid(row = 0, column = 0, padx = 10, pady = 10, sticky = 'nw')
 
     # Value Select
-    hsptlValue_entry = ttk.Entry(root, textvariable = hsptlValue, width = 23)
+    hsptlValue_entry = ttk.Entry(inputFrame, textvariable = hsptlValue, width = 23)
     hsptlValue_entry.grid(row = 1, column = 1, padx = 10, pady = 10, sticky = 'nw')
 
-    hsptlValue_label = tk.Label(root, text = "Hospital Value")
+    hsptlValue_label = tk.Label(inputFrame, text = "Hospital Value:")
     hsptlValue_label.grid(row = 1, column = 0, padx = 10, pady = 10, sticky = 'nw')
 
-    clncValue_entry = ttk.Entry(root, textvariable = clncValue, width = 23)
+    clncValue_entry = ttk.Entry(inputFrame, textvariable = clncValue, width = 23)
     clncValue_entry.grid(row = 2, column = 1, padx = 10, pady = 10, sticky = 'nw')
 
-    clncValue_label = tk.Label(root, text = "Clinic Value")
+    clncValue_label = tk.Label(inputFrame, text = "Clinic Value:")
     clncValue_label.grid(row = 2, column = 0, padx = 10, pady = 10, sticky = 'nw')
 
     # File name label
-    file_label = tk.Label(root, text = 'File Selected:')
+    file_label = tk.Label(inputFrame, text = 'File Selected:')
     file_label.grid(row = 3, column = 0, padx = 10, pady = 10, sticky = 'nw')
 
-    fileVarlabel = tk.Label(root, textvariable = fileVar)
+    fileVarlabel = tk.Label(inputFrame, textvariable = fileVar)
     fileVarlabel.grid(row = 3, column = 1, padx = 10, pady =10, sticky = 'nw')
+
+    # Output Label
+    output_label = tk.Label(inputFrame, text = "Output Destination:")
+    output_label.grid(row = 4, column = 0, padx = 10, pady = 10, sticky = 'nw')
+
+    destination_label = tk.Label(inputFrame, textvariable = outVar)
+    destination_label.grid(row = 4, column = 1, padx = 10, pady = 10, sticky = 'nw')
 
     # Status label
     stsLabel = tk.Label(root, textvariable = statusVar)
-    stsLabel.grid(row = 4, column = 0, padx = 10, pady = 10, sticky = 'nw')
+    stsLabel.grid(row = 5, column = 0, padx = 10, pady = 10, sticky = 'nw')
 
     #dumbbutton = tk.Button(root, text = 'dumb', command = printy)
     #dumbbutton.grid(row = 4, column = 1, padx = 10, pady =10, sticky = 'nw')
-
-    # File Preview
-
 
     root.columnconfigure([0, 1], weight = 1)
     #root.rowconfigure([1, 0], weight = 1)
