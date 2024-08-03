f9e63262b15645ac7e11c0b8970d6ca49c5780f1
Ethan Wiens <ethanjwiens@gmail.com>
2024-08-03 15:56:24 -0500

    import pdf

--- funk/__init__.py
+++ funk/__init__.py
@@ -1,4 +1,5 @@
 from .gooey import create_gui
+from .peedeeeff import open_pdf, which_pdf, pdf_report
 from .spreadsheet import open_file, which_csv, csv_report
 from .web import login, which_html, html_report
 
