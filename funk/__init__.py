a0d7608c5a844b64629b4d531d11621a2412c89f
ethan wiens <ethanjwiens@gmail.com>
2024-08-06 16:19:15 -0500

    remove creds from init

--- funk/__init__.py
+++ funk/__init__.py
@@ -2,10 +2,3 @@ from .gooey import create_gui
 from .peedeeeff import open_pdf, which_pdf, pdf_report
 from .spreadsheet import open_file, which_csv, csv_report
 from .web import login, which_html, html_report, save
-
-try:
-    from .creds import username, password
-    credsPresent = True
-except ImportError:
-    credPresent = False
-    pass
